845e8646d5857a840dc1dc6303a1520e8db92439
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-03-20 17:54:01 +0700

    init game

---
+++ game.py
@@ -0,0 +1,11 @@
+import pygame, sys
+
+# Khởi tạo game
+pygame.init()
+
+# Set up chiều dài, chiều rộng khung hình
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# Khởi tạo màn hình game
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
