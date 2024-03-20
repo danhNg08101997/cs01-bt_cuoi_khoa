f08d86860c8f6515f3fb4537c56683052474360a
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-03-20 22:44:35 +0700

    khoi tao source pygame

--- game.py
+++ game.py
@@ -9,3 +9,30 @@ SCREEN_HEIGHT = 720
 
 # Khởi tạo màn hình game
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Tiêu đề game
+pygame.display.set_caption('Metal Slug')
+
+# Set up bộ đếm thời gian
+clock = pygame.time.Clock()
+
+# Tạo background
+bg_game = pygame.image.load('./images/bkgd.png')
+bg_game_rect = bg_game.get_rect()
+bg_game = pygame.transform.scale(bg_game,(bg_game_rect.width, SCREEN_HEIGHT))
+
+# Tạo vòng lặp game
+running = True
+while running:
+    clock.tick(60)
+    
+    screen.fill((255,255,255))
+    
+    for event in pygame.event.get():
+        # Xử lý thoát game
+        if event.type == pygame.QUIT:
+            running = False
+            
+    pygame.display.flip()
+    
+pygame.quit()
+sys.exit()
