dcbf404c2dc714aa8c863d44736842a31f1b4924
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-03-26 07:00:55 +0700

    background music

--- game.py
+++ game.py
@@ -9,8 +9,14 @@ SCREEN_HEIGHT = 720
 
 # Khởi tạo màn hình game
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 # Tiêu đề game
 pygame.display.set_caption('Metal Slug')
+
+# Thiết lập âm thanh nhạc nền
+bg_music = pygame.mixer.music.load('./sounds/music.mp3')
+bg_music = pygame.mixer.music.set_volume(0.3)
+bg_music = pygame.mixer.music.play(-1)
 
 # Set up bộ đếm thời gian
 clock = pygame.time.Clock()
